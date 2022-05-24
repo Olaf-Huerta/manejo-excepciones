e3390231cd87befaf51f5cdf2a32d8e6500a7c33
Olaf Huerta <l21211966@tectijuana.edu.mx>
2022-05-24 12:15:11 -0700

    programa manejo de excepciones
    ea

---
+++ manejoExcepciones/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace manejoExcepciones
+{
+    class Program
+    {
+        public class Cuadrado
+        {
+            int N;
+
+            public Cuadrado(int N)
+            {
+                this.N = N;
+            }
+
+            public int Operacion(int N)
+            {
+
+                return N * N;
+            }
+
+            ~Cuadrado()
+            {
+                Console.WriteLine("Memoria Liberada");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int N, valor;
+
+            Console.WriteLine("-----Cuadrado de un valor entero-----");
+            Console.WriteLine();
+
+            try
+            {
+                Console.Write("Ingrese un valor entero: ");
+                N = int.Parse(Console.ReadLine());
+
+                Cuadrado c = new Cuadrado(N);
+
+                valor = c.Operacion(N);
+
+                Console.WriteLine("Valor ingresado al cuadrado: " + valor);
+
+            }
+            catch (FormatException x)
+            {
+                Console.WriteLine(x.Message);
+            }
+            catch (OverflowException x)
+            {
+                Console.WriteLine(x.Message);
+            }
+            catch (Exception x)
+            {
+                Console.WriteLine(x.Message);
+            }
+
+            finally
+            {
+                Console.ReadKey();
+                Console.Clear();
+            }
+        }
+    }
+}
